8965018d7a581cc6a2e86b083ab302f1e781b829
Gloria Deng <gloria@azuqua.com>
2015-11-14 13:35:07 -0800

    update version;

--- app.js
+++ app.js
@@ -3,11 +3,7 @@
 var app = require("express")();
 app.set("port", 9000);
 
-var wholeMill = require("./lib")(app);
-
-// app.get("/admin/editor", function(req, res) {
-//   res.render("editor");
-// });
+require("./lib")(app);
 
 var server = app.listen(app.get("port"));
 console.log("Server listening on port " + app.get("port"));
